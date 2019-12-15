e7c3a63d7dc3cbd044b0f9f1ebc71b65b3caa2f8
edoardomazzella <edoardo.mazzella91@gmail.com>
2019-12-15 20:47:07 +0100

    big update

--- helpers.py
+++ helpers.py
@@ -37,3 +37,57 @@ def line_visualization(binary_warped, nonzerox, nonzeroy, left_lane_inds, right_
     
     # View your output
     plt.imshow(result)
+
+# Curvature computation
+def compute_curvature(img, allx, ally):
+    ym_per_pix = 20 / 720
+    xm_per_pix = 3.7 / 800
+    
+    fit_cr = np.polyfit(ally * ym_per_pix, allx * xm_per_pix, 2)
+    curvature = ((1 + (2*fit_cr[0]*img.shape[0]*ym_per_pix + fit_cr[1])**2)**1.5) / np.absolute(2*fit_cr[0])
+    
+    return curvature
+
+# Function to fit polynomial, used by subsequent functions
+def fit_poly(img_shape, allx, ally):
+    ### Fit a second order polynomial to each with np.polyfit() ###
+    fit = np.polyfit(ally, allx, 2)
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+    ### Calc polynomial using ploty and fit ###
+    fitx = fit[0]*ploty**2 + fit[1]*ploty + fit[2]
+
+    return fit, fitx, ploty
+
+# Function to draw the lane
+def drawLane(undist, binary_warped, ploty, left_fitx, right_fitx, curvature, distance, Minv):
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (color_warp.shape[1], color_warp.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    
+    result = cv2.putText(result, "Radius of the curvature: %.0fm" % curvature, (50, 50), cv2.FONT_HERSHEY_SIMPLEX,  
+                   1, (255, 0, 0), 2, cv2.LINE_AA)
+    if(distance < 0):
+        result = cv2.putText(result, "Distance: %.2fm on the left of center" % np.absolute(distance), (50, 100),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    elif(distance > 0):
+        result = cv2.putText(result, "Distance: %.2fm on the right of center" % np.absolute(distance), (50, 100),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    else:
+        result = cv2.putText(result, "Distance: %.2fm" % np.absolute(distance), (50, 100),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    return result
